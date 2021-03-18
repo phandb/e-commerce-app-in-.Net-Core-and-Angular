581de1a082764ca164fccd578a717d4b656cdecf
Dalton <phandb72@gmail.com>
2021-03-17 19:11:10 -0500

    API Errors Handling

--- API/Startup.cs
+++ API/Startup.cs
@@ -8,6 +8,11 @@ using Infrastructure.Data;
 using Core.Interfaces;
 using API.Helpers;
 using API.MiddleWare;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using API.Errors;
+using Microsoft.OpenApi.Models;
+using API.Extensions;
 
 namespace API
 {
@@ -20,37 +25,37 @@ namespace API
             _config = config;
         }
 
-       // public IConfiguration Configuration { get; }
+        // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+           
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseMiddleware<ExceptionMiddleware>();
-            
-            if (env.IsDevelopment())
-            {
-                // app.UseDeveloperExceptionPage();
-               // app.UseSwagger();
-               // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+
+           // app.UseDeveloperExceptionPage();
+            app.UseSwaggerDocumentation();
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            
+
             app.UseStaticFiles();
 
             app.UseAuthorization();
