e244b112f262317e9f8bd780313e52c7838f51ba
Dalton <phandb72@gmail.com>
2021-05-02 18:28:59 -0500

    Add Angular client to project

--- API/Helpers/SwaggerServiceExtensions.cs
+++ API/Helpers/SwaggerServiceExtensions.cs
@@ -20,8 +20,8 @@ namespace API.Extensions
         public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
         {
             app.UseSwagger();
-            app.UseSwaggerUI(c => {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Skinet API v1");});
+            app.UseSwaggerUI(c => 
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             
 
             return app;
