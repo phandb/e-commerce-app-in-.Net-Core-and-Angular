8b01d1ea67d3fc1391a7285ab58c1401d27619f1
Dalton <phandb72@gmail.com>
2021-05-13 07:44:20 -0500

    Fixing Errors in Console and Bugs

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
@@ -11,6 +11,7 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
+  @ViewChild('search', {static: true}) searchTerm: ElementRef;
   products: IProduct[];
   brands: IBrand[];
   types: IType[];
@@ -74,12 +75,14 @@ export class ShopComponent implements OnInit {
   // tslint:disable-next-line: typedef
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   // tslint:disable-next-line: typedef
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -92,8 +95,25 @@ export class ShopComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   onPageChanged(event: any) {
-    // this.shopParams.pageNumber = event.page;
-    this.shopParams.pageNumber = event;
+    
+    // Check to see if a page is really changed
+    if (this.shopParams.pageNumber !== event) {
+      // this.shopParams.pageNumber = event.page;
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+    
+  }
+
+  onSearch() {
+    this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  onReset() {
+    this.searchTerm.nativeElement.value = "";
+    this.shopParams = new ShopParams();
     this.getProducts();
   }
 
